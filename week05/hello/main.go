5f55ae00b279d4969c99e259e36cbb27af26cbd9
yoojae <wodn061115@@naver.com>
2025-09-22 15:38:31 +0900

    v0.2 hello/main.go & week05.txt update

--- week05/hello/main.go
+++ week05/hello/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	name := "Go"
-	fmt.Println("Hello ", name)
+	// name := "Go"
+	// fmt.Println("Hello ", name)
+
+	number := 7
+	fmt.Printf("Number is %d\n", number)
 }
